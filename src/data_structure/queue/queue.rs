65c7d5fa2ae04b1bd6c90855a32159e9deeb729b
miinhho <mjang6988@gmail.com>
2025-04-29 23:02:45 +0900

    test: Add Queue case
    - change comment format in queue for rust (///)

--- src/data_structure/queue/queue.rs
+++ src/data_structure/queue/queue.rs
@@ -6,34 +6,34 @@ pub struct Queue<T> {
 }
 
 impl<T> Queue<T> {
-    // Creates a new empty Queue
+    /// Creates a new empty Queue
     pub fn new() -> Queue<T> {
         Queue {
             elements: LinkedList::new(),
         }
     }
 
-    // Adds an element to the back of the queue
+    /// Adds an element to the back of the queue
     pub fn enqueue(&mut self, value: T) {
         self.elements.push_back(value)
     }
 
-    // Removes and returns the front element from the queue, or None is empty
+    /// Removes and returns the front element from the queue, or None is empty
     pub fn dequeue(&mut self) -> Option<T> {
         self.elements.pop_front()
     }
 
-    // Returns a reference to the front element of the queue, or None if empty
+    /// Returns a reference to the front element of the queue, or None if empty
     pub fn peek_front(&self) -> Option<&T> {
         self.elements.front()
     }
 
-    // Returns a reference to the back element of the queue, or None if empty
+    /// Returns a reference to the back element of the queue, or None if empty
     pub fn peek_back(&self) -> Option<&T> {
         self.elements.back()
     }
 
-    // Returns the number of elements in the queue
+    /// Returns the number of elements in the queue
     pub fn len(&self) -> usize {
         self.elements.len()
     }
@@ -43,6 +43,7 @@ impl<T> Queue<T> {
         self.elements.is_empty()
     }
 
+    /// Clears all elements from the queue
     pub fn drain(&mut self) {
         self.elements.clear();
     }
@@ -52,5 +53,34 @@ impl<T> Queue<T> {
 impl<T> Default for Queue<T> {
     fn default() -> Queue<T> {
         Queue::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Queue;
+
+    #[test]
+    fn test_queue_functionality() {
+        let mut queue: Queue<usize> = Queue::default();
+
+        assert!(queue.is_empty());
+        queue.enqueue(8);
+        queue.enqueue(16);
+        assert!(!queue.is_empty());
+        assert_eq!(queue.len(), 2);
+
+        assert_eq!(queue.peek_front(), Some(&8));
+        assert_eq!(queue.peek_back(), Some(&16));
+
+        assert_eq!(queue.dequeue(), Some(8));
+        assert_eq!(queue.len(), 1);
+        assert_eq!(queue.peek_front(), Some(&16));
+        assert_eq!(queue.peek_back(), Some(&16));
+
+        queue.drain();
+        assert!(queue.is_empty());
+        assert_eq!(queue.len(), 0);
+        assert_eq!(queue.dequeue(), None);
     }
 }
