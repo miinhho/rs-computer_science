d233140a87efd5b731da41af74e0c685ef5340bc
miinhho <mjang6988@gmail.com>
2025-04-29 22:30:31 +0900

    test: Add insert_at_head, insert_at_ith case
    - Remove println! fn in insert_at_ith

--- src/data_structure/linked_list/linked_list.rs
+++ src/data_structure/linked_list/linked_list.rs
@@ -85,7 +85,6 @@ impl<T> LinkedList<T> {
                 node.next = Some(ith_node);
                 if let Some(p) = (*ith_node.as_ptr()).prev {
                     let node_ptr = NonNull::new(Box::into_raw(node));
-                    println!("{:?}", (*p.as_ptr()).next);
                     (*p.as_ptr()).next = node_ptr;
                     (*ith_node.as_ptr()).prev = node_ptr;
                     self.length += 1;
@@ -218,6 +217,128 @@ mod tests {
         match list.get(1) {
             Some(val) => assert_eq!(*val, second_value),
             None => panic!("Expected to find {second_value} at index 1"),
+        }
+    }
+
+    #[test]
+    fn insert_at_head_works() {
+        let mut list = LinkedList::<i32>::new();
+        let second_value = 2;
+        list.insert_at_head(1);
+        list.insert_at_head(second_value);
+        println!("Linked List is {list}");
+        match list.get(0) {
+            Some(val) => assert_eq!(*val, second_value),
+            None => panic!("Expected to find {second_value} at index 0"),
+        }
+    }
+
+    #[test]
+    fn insert_at_ith_can_add_to_tail() {
+        let mut list = LinkedList::<i32>::new();
+        let second_value = 2;
+        list.insert_at_ith(0, 0);
+        list.insert_at_ith(1, second_value);
+        println!("Linked List is {list}");
+        match list.get(1) {
+            Some(val) => assert_eq!(*val, second_value),
+            None => panic!("Expected to find {second_value} at index 1"),
+        }
+    }
+
+    #[test]
+    fn insert_at_ith_can_add_to_head() {
+        let mut list = LinkedList::<i32>::new();
+        let second_value = 2;
+        list.insert_at_ith(0, 1);
+        list.insert_at_ith(0, second_value);
+        println!("Linked List is {list}");
+        match list.get(0) {
+            Some(val) => assert_eq!(*val, second_value),
+            None => panic!("Expected to find {second_value} at index 0"),
+        }
+    }
+
+    #[test]
+    fn insert_at_ith_can_add_to_middle() {
+        let mut list = LinkedList::<i32>::new();
+        let second_value = 2;
+        let third_value = 3;
+        list.insert_at_ith(0, 1);
+        list.insert_at_ith(1, second_value);
+        list.insert_at_ith(1, third_value);
+        println!("Linked List is {list}");
+        match list.get(1) {
+            Some(val) => assert_eq!(*val, third_value),
+            None => panic!("Expected to find {third_value} at index 1"),
+        }
+
+        match list.get(2) {
+            Some(val) => assert_eq!(*val, second_value),
+            None => panic!("Expected to find {second_value} at index 1"),
+        }
+    }
+
+    #[test]
+    fn insert_at_ith_and_delete_at_ith_in_the_middle() {
+        // Insert and delete in the middle of the list to ensure pointers are updated correctly
+        let mut list = LinkedList::<i32>::new();
+        let first_value = 0;
+        let second_value = 1;
+        let third_value = 2;
+        let fourth_value = 3;
+
+        list.insert_at_ith(0, first_value);
+        list.insert_at_ith(1, fourth_value);
+        list.insert_at_ith(1, third_value);
+        list.insert_at_ith(1, second_value);
+
+        list.delete_ith(2);
+        list.insert_at_ith(2, third_value);
+
+        for (i, expected) in [
+            (0, first_value),
+            (1, second_value),
+            (2, third_value),
+            (3, fourth_value),
+        ] {
+            match list.get(i) {
+                Some(val) => assert_eq!(*val, expected),
+                None => panic!("Expected to find {expected} at index {i}"),
+            }
+        }
+    }
+
+    #[test]
+    fn insert_at_ith_and_delete_ith_work_over_many_iterations() {
+        let mut list = LinkedList::<i32>::new();
+        for i in 0..100 {
+            list.insert_at_ith(i, i.try_into().unwrap());
+        }
+
+        for i in 0..50 {
+            println!("list.length {}", list.length);
+            if i % 2 == 0 {
+                list.delete_ith(i);
+            }
+        }
+
+        assert_eq!(list.length, 75);
+
+        // Insert even numbers back
+        for i in 0..50 {
+            if i % 2 == 0 {
+                list.insert_at_ith(i, i.try_into().unwrap());
+            }
+        }
+
+        assert_eq!(list.length, 100);
+
+        // Ensure numbers were added back and we're able to traverse nodes
+        if let Some(val) = list.get(78) {
+            assert_eq!(*val, 78);
+        } else {
+            panic!("Expected to find 78 at index 78");
         }
     }
 }
